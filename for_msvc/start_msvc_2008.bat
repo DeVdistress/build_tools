74a769b1529b2071b4c6fc463c2b81aad2c431ed
DeV <DeVdistress@gmail.com>
2021-05-12 16:32:46 +0300

    + a bit of fix

--- for_msvc/start_msvc_2008.bat
+++ for_msvc/start_msvc_2008.bat
@@ -3,7 +3,7 @@ ECHO ON
 :: -------------MSVC-------------
 CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 
-CALL set_paths_2008.bat "C:\Python\Python27\" "C:\Program Files (x86)\Microsoft DirectX SDK (March 2009)"
+CALL set_paths_2008.bat "C:\Python\Python27\" "C:\Program Files (x86)\Microsoft DirectX SDK (March 2009)\"
 
 CALL c:\disk_p\mount_disk_p.bat
 
--- for_msvc/start_msvc_2019.bat
+++ for_msvc/start_msvc_2019.bat
@@ -3,7 +3,7 @@ ECHO ON
 :: -------------MSVC-------------
 CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 
-CALL set_paths_2019.bat "C:\Python\Python27\" "%PROGRAMFILES(X86)%\Microsoft DirectX SDK (June 2010)"
+CALL set_paths_2019.bat "C:\Python\Python27\" "%PROGRAMFILES(X86)%\Microsoft DirectX SDK (June 2010)\"
 
 CALL c:\disk_p\mount_disk_p.bat
 
