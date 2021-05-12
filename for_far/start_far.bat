a49ef5e16d96e4b84a745bb1df2515e01dcb7d7d
DeV <devdistress@gmail.com>
2021-05-12 20:29:08 +0300

    + added fix for far

--- for_far/start_far.bat
+++ for_far/start_far.bat
@@ -1,20 +1,20 @@
 ECHO ON
 
-call set_paths.bat "C:\Python\Python27\" "C:\Program Files (x86)\Microsoft DirectX SDK (March 2009)"
+CALL set_paths.bat "C:\Python\Python27\" "C:\Program Files (x86)\Microsoft DirectX SDK (March 2009)"
 :: call set_paths.bat C:\Python\Python39\
 
 :: -------------MSVC-------------
-call "%VS80COMNTOOLS%\..\..\VC\vcvarsall.bat"
-:: call "%PROGRAMFILES(X86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
-:: call "%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-:: call "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" -arch=amd64
+CALL "%VS80COMNTOOLS%\..\..\VC\vcvarsall.bat"
+:: CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+:: CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+:: CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" -arch=amd64
+:: CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 
 :: PAUSE
 
-call c:\disk_p\mount_disk_p.bat
+CALL c:\disk_p\mount_disk_p.bat
 
 ::PAUSE
 
-cd P:\
-
+CD P:\
 %COMSPEC% /k "%MY_FAR%\Far.exe mode con lines=9999"
