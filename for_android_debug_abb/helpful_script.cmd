7d253a5e38d114bf44c3e26b597f200ef23e9153
DeV <devdistress@gmail.com>
2021-09-07 13:40:42 +0300

    + added JDK path

--- for_android_debug_abb/helpful_script.cmd
+++ for_android_debug_abb/helpful_script.cmd
@@ -1,10 +1,11 @@
 adb uninstall %4
 
+SET PATH_TO_JDK=%5
 SET APKS_NAME=%1
 SET BUNDLE_NAME=%2
 SET BUNDLE_TOOL_NAME=%3
 del /F /Q %APKS_NAME%
 
-SET CMD_JAVA=jdk-11.0.8\bin\java.exe -jar %BUNDLE_TOOL_NAME%
+SET CMD_JAVA=%PATH_TO_JDK%\bin\java.exe -jar %BUNDLE_TOOL_NAME%
 call %CMD_JAVA% build-apks --bundle=%BUNDLE_NAME% --output=%APKS_NAME% --local-testing
 call %CMD_JAVA% install-apks --apks=%APKS_NAME% --adb=adb.exe
--- for_android_debug_abb/install.bat
+++ for_android_debug_abb/install.bat
@@ -1,1 +1,1 @@
-call helpful_script.cmd  NAME1.apks NAME2.aab bundletool-all-1.8.0.jar com.wb.goog.legoswtfa_test
+call helpful_script.cmd  NAME1.apks NAME2.aab bundletool-all-1.8.0.jar com.wb.goog.legoswtfa_test W:\LegoTFA\global\Utils\Windows\Java\jdk-16.0.2
