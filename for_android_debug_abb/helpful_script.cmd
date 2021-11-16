9d98439aea2539b6bee510f204c7a8e2498a2a24
DeV <devdistress@gmail.com>
2021-11-16 12:57:50 +0300

    + added a bit of cement

--- for_android_debug_abb/helpful_script.cmd
+++ for_android_debug_abb/helpful_script.cmd
@@ -1,16 +1,28 @@
+@echo off 
+
 SET PATH_TO_JDK=%5
 SET APKS_NAME=%3
 SET BUNDLE_NAME=%1
 SET BUNDLE_TOOL_NAME=%4
 SET PACKAGE_NAME=%2
 
+:: if you no need to build, just commit it
+::SET var=build_new_apk
+
+echo Process uninstall apk is starting
 call adb uninstall %PACKAGE_NAME%
 
 SET CMD_JAVA=%PATH_TO_JDK%\bin\java.exe -jar %BUNDLE_TOOL_NAME%
 
-del /F /Q %APKS_NAME%
-call %CMD_JAVA% build-apks --bundle=%BUNDLE_NAME% --output=%APKS_NAME% --local-testing
+IF DEFINED var (
+	IF "%var%"=="build_new_apk" (
+		echo Process build of new apk is starting
+		del /F /Q %APKS_NAME%
+		call %CMD_JAVA% build-apks --bundle=%BUNDLE_NAME% --output=%APKS_NAME% --local-testing
+	)
+)
 
+echo Process install apk is starting
 call %CMD_JAVA% install-apks --apks=%APKS_NAME% --adb=adb.exe
 
 :: start app
