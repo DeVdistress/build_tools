533fbd7ead7162c20bc92fc671a6d4405237bf63
DeV <devdistress@gmail.com>
2021-12-02 15:00:30 +0300

    + for test

--- mount_disk_z.bat
+++ mount_disk_z.bat
@@ -1,3 +1,7 @@
 subst z: /D
+subst q: /D
+::subst z: W:\simly_lego_jurassic
+::subst q: W:\LegoJurassic\global
 subst z: W:\LegoTFA
-:: subst z: W:\LegoJurassic
+::subst z: W:\LegoJurassic
+::subst z: W:\MarvelSuperHero
