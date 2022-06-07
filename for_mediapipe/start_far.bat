ae33a7e1f070e77f45c9d28e0a166c808c213c8a
DeVdistress <devdistress@gmail.com>
2022-06-08 00:06:03 +0300

    + fixed for right way to build

--- for_mediapipe/start_far.bat
+++ for_mediapipe/start_far.bat
@@ -2,18 +2,12 @@ ECHO ON
 
 CALL set_paths.bat "C:\Python\Python39" "C:\msys64" "C:\msys64\usr\bin" "C:\bazel" "C:\nuget" "C:\opencv"
 
-:: -------------MSVC-------------
-:: CALL "%VS80COMNTOOLS%\..\..\VC\vcvarsall.bat"
-:: CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
-:: CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-:: CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" -arch=amd64
-CALL "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-
- PAUSE
+:: PAUSE
 
 CALL ..\mount_disk_p.bat
 
-::PAUSE
-
-CD P:\
+:: PAUSE
+P:
+CD P:\MediaPipeUnityPlugin
+CALL python.exe build.py build --desktop cpu -v
 %COMSPEC% /k "%MY_FAR%\Far.exe mode con lines=9999"
