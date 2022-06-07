8b0b1e73b88099b5cee5ec5ba48bc8c5ccce80f7
DeVdistress <devdistress@gmail.com>
2022-06-08 00:19:35 +0300

    + added pause

--- for_mediapipe/start_far.bat
+++ for_mediapipe/start_far.bat
@@ -2,12 +2,10 @@ ECHO ON
 
 CALL set_paths.bat "C:\Python\Python39" "C:\msys64" "C:\msys64\usr\bin" "C:\bazel" "C:\nuget" "C:\opencv"
 
-:: PAUSE
-
 CALL ..\mount_disk_p.bat
 
-:: PAUSE
 P:
 CD P:\MediaPipeUnityPlugin
 CALL python.exe build.py build --desktop cpu -v
+PAUSE
 %COMSPEC% /k "%MY_FAR%\Far.exe mode con lines=9999"
