6a749129a63b60a257d72db475a9a2c88b11f726
DeV <devdistress@gmail.com>
2022-06-10 19:32:36 +0300

    + added minor fixes

--- for_mediapipe/start_far.bat
+++ for_mediapipe/start_far.bat
@@ -1,6 +1,6 @@
 ECHO ON
 
-CALL set_paths.bat "C:\Python\Python39" "C:\msys64" "C:\msys64\usr\bin" "C:\bazel" "C:\nuget" "C:\opencv" "Z:\global\Utils\Windows\Java\jdk-11.0.12" "Z:\global\Lib\android\android-ndk" "Z:\global\Lib\android\android-sdk"
+CALL set_paths.bat "C:\Python\Python39" "C:\msys64" "C:\msys64\usr\bin" "C:\bazel" "C:\nuget" "C:\opencv" "Z:\global\Utils\Windows\Java\jdk-11.0.12" "Z:\global\Lib\android\android-sdk" "Z:\global\Lib\android\android-sdk\ndk\21.4.7075529" 
 
 CALL ..\mount_disk_p.bat
 CALL ..\mount_disk_w.bat
@@ -13,7 +13,7 @@ CD P:\MediaPipeUnityPlugin
 :: CALL python.exe build.py build --desktop cpu -v
 
 :: Build for Android
-CALL python.exe build.py build --android arm64 --android_ndk_api_level 21 --cxxopt=-std=c++14 -vv
+CALL python.exe build.py build --android arm64 --android_ndk_api_level 21 -vv
 
 PAUSE
 %COMSPEC% /k "%MY_FAR%\Far.exe mode con lines=9999"
