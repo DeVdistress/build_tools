c08230cad7ad60c2b9e5b16d4e1173bd4a19781a
DeV <devdistress@gmail.com>
2022-07-19 19:56:41 +0300

    + added tfa

--- for_android_debug_abb/start_install_not_on_lephone.bat
+++ for_android_debug_abb/start_install_not_on_lephone.bat
@@ -1,1 +1,5 @@
-call C:\Python\Python39\python install_not_on_lephone.py 57.apk  com.wb.goog.legojurassicworld_test com.wb.goog.legojurassicworld.GameActivity
+:: for Jurassic
+:: call C:\Python\Python39\python install_not_on_lephone.py 65.apk  com.wb.goog.legojurassicworld_test com.wb.goog.legojurassicworld.GameActivity
+
+:: for TFA
+call C:\Python\Python39\python install_not_on_lephone.py 31.apk  com.wb.goog.legoswtfa_test com.wb.goog.legoswtfa.GameActivity
--- for_android_debug_abb/start_install_on_many_devices.bat
+++ for_android_debug_abb/start_install_on_many_devices.bat
@@ -1,1 +1,5 @@
-call C:\Python\Python39\python install_on_many_devices.py 57.apk  com.wb.goog.legojurassicworld_test com.wb.goog.legojurassicworld.GameActivity
+:: for Jurassic
+:: call C:\Python\Python39\python install_on_many_devices.py 75.apk com.wb.goog.legojurassicworld_test com.wb.goog.legojurassicworld.GameActivity
+
+:: for TFA
+call C:\Python\Python39\python install_on_many_devices.py 31.apk  com.wb.goog.legoswtfa_test com.wb.goog.legoswtfa.GameActivity
